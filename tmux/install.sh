e2e6f37b8297b4f6026a43f0b32a4784cb089f3f
Tetsutaro Maruyama <tetsutaro.maruyama@gmail.com>
2021-10-24 23:54:40 +0900

    compatible with Mac OSX

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,10 +1,21 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 os=$(uname -s)
 if [ -z $(command -v tmux) ]; then
     if [ "${os}" == "Linux" ]; then
         sudo apt install tmux
     else
+        # https://github.com/tmux/tmux/issues/2262
+        curl -OL https://gist.githubusercontent.com/nicm/ea9cf3c93f22e0246ec858122d9abea1/raw/37ae29fc86e88b48dbc8a674478ad3e7a009f357/tmux-256color
+        echo '8f259a31649900b9a8f71cbc28be762aa55206316d33d51fd8d08e4275b5f6a3  tmux-256color' | shasum -a 256 -c
+        if [ $? == 0 ]; then
+            /usr/bin/tic -x tmux-256color
+        else
+            echo "tmux-256color checksum has changed"
+            exit 1
+        fi
+        rm -f tmux-256color
+        # install tmux via homebrew
         brew install tmux
     fi
 else
