7df3f347719518f58df4dc6f766456f5292c7c8b
Tetsutaro Maruyama <tetsutaro.maruyama@gmail.com>
2023-11-27 07:47:12 +0900

    JupyterLab 4.0.9 and jupyterlab-desktop

--- jupyterlab/README.md
+++ jupyterlab/README.md
@@ -1,48 +1,19 @@
-# JupyterLab on pipx & Yet Another JupyterLab App
-
-Yet Another JupyterLab App: The simplest desktop application only for [JupyterLab](https://github.com/jupyterlab/jupyterlab), based on [Electron](https://www.electronjs.org/)
-
-![](screenshot.png)
-
-## Motivation for create original app for JupyterLab
-
-As you know, [JupyterLab App](https://github.com/jupyterlab/jupyterlab_app) is the awesome desktop application for JupyterLab, developed by [Project Jupyer](https://jupyter.org/).
-
-[JupyterLab App](https://github.com/jupyterlab/jupyterlab_app) contains the desktop application (JupyterLab client) based on Electron and Python virtual environment (venv) with the JupyterLab server.
-
-But it is too much for me. I want to run the JupyterLab server on my own venv. I just need a simple application which only access to `http://localhost:8888/lab`.
-
-On the other hand, [Electron](https://www.electronjs.org/) provides a great sample implementation: [electron-quick-start](https://github.com/electron/electron-quick-start). It is a simple desktop application that only displays `index.html` with `electron.BrowserWindow().loadFile('index.html')`.
-
-That is very close to what I'm looking for. The app I need is doing `electron.BrowserWindow().loadURL('http://localhost:8888/lab')` instead of `loadFile()`.
-
-So, I made it.
+# JupyterLab on pipx
 
 ## Necessities
 
-* [Node.js](https://nodejs.org/)
-    * nodenv and its plugins are installed in [zsh](/zsh/)
-    * Install Node.js with nodenv and set it as global use
-        * `> nodenv install --list`
-        * `> nodenv install x.x.x`
-        * `> nodenv global x.x.x`
-* [yarn](https://yarnpkg.com/)
-    * `> npm install -g yarn`
-        * `npm` is automatically installed when you install Node.js
-        * Don't forget to add `-g` option
 * pipx
     * pipx is installed in [zsh](/zsh/)
 
 ## What this does
 
-* install jq (if it has not been installed)
 * install jupyterlab and packages with pipx (if they have not been installed)
     * packages: python-lsp-server[all], jupyterlab-lsp
 * create symbolic links of client config file
     * `user-settings` -> `~/.jupyter/lab/user-settings`
 * create symbolic link of server config file
     * `jupyter_lab_config.py.linux` / `jupyter_lab_config.py.macos` -> `${pipx_jupyterlab_etc}/jupyter_lab_config.py`
-* build and install app (if it has not been installed)
+* install jupyterlab-desktop
 
 ## What to do
 
--- jupyterlab/install.sh
+++ jupyterlab/install.sh
@@ -12,44 +12,13 @@ if [[ ! -f ${PIPX_PATH} ]]; then
 else
     echo "pipx is already installed"
 fi
-if [[ ! -d ${HOME}/.nodenv ]]; then
-    echo "install nodenv first"
-    exit 1
-else
-    echo "nodenv is already installed"
-fi
-if [[ -z $(command -v npm) ]]; then
-    echo "install Node.js first"
-    exit 1
-else
-    echo "Node.js is already installed"
-fi
-if [[ -z $(command -v yarn) ]]; then
-    echo "install yarn"
-    npm install -g yarn
-else
-    echo "yarn is already installed"
-fi
-if [[ -z $(command -v jq) ]]; then
-    if [[ "${os}" == "Linux" ]]; then
-        sudo apt install jq
-    else
-        brew install jq
-    fi
-    rehash
-else
-    echo "jq is already installed"
-fi
-
-## get version
-VERSION=$(cat package.json | jq -r .version)
 
 ## jupyterlab on pipx
 if [[ ! -d "${HOME}/.local/pipx/venvs/jupyterlab" ]]; then
     pipx install jupyterlab
     pipx inject jupyterlab pip
-    pipx runpip jupyterlab install black flake8 python-lsp-server jupyterlab-code-formatter jupyterlab-lsp jupyterlab-templates jupyterlab-widgets ipywidgets
-    pipx run --spec jupyterlab jupyter labextension install @jupyter-widgets/jupyterlab-manager @jupyterlab/toc jupyterlab_templates
+    pipx runpip jupyterlab install black flake8 python-lsp-server jupyterlab-code-formatter jupyterlab-lsp jupyterlab-templates jupyterlab-widgets ipywidgets ipympl
+    pipx run --spec jupyterlab jupyter labextension install @jupyter-widgets/jupyterlab-manager jupyterlab_templates jupyter-matplotlib
     pipx run --spec jupyterlab jupyter lab build
     pipx run --spec jupyterlab jupyter lab clean
 else
@@ -80,31 +49,29 @@ if [[ ${os} == "Linux" ]]; then
 elif [[ ${os} == "Darwin" ]]; then
     cp ${PWD}/jupyter_lab_config.py.macos ${HOME}/.jupyter/jupyter_lab_config.py
 fi
-echo "c.JupyterLabTemplates.template_dirs = ['${HOME}/.jupyter/templates']" >> ${HOME}/.jupyter/jupyter_lab_config.py
+echo "c.JupyterLabTemplates.template_dirs = ['$(echo ${HOME})/.jupyter/templates']" >> ${HOME}/.jupyter/jupyter_lab_config.py
 ln -sf ${PWD}/templates ${HOME}/.jupyter/templates
 ln -sf ${PWD}/flake8 ${HOME}/.jupyter/flake8
 ln -sf ${PWD}/user-settings ${HOME}/.jupyter/lab/user-settings
 
 ## check the app is installed
 if [[ "${os}" == "Linux" ]]; then
-    if [[ "$(snap list | grep jupyterlab)" != "" ]]; then
-        echo "JupyterLab.app is already installed"
+    if [[ -z $(command -v jlab) ]]; then
+        echo "jupyterlab-desktop is already installed"
         exit 0
     fi
+    wget https://github.com/jupyterlab/jupyterlab-desktop/releases/latest/download/JupyterLab-Setup-Debian.deb
+    sudo dpkg -i JupyterLab-Setup-Debian.deb
+    rm -f JupyterLab-Setup-Debian.deb
 else
     if [[ -d "/Applications/JupyterLab.app" ]]; then
-        echo "JupyterLab.app is already installed"
+        echo "jupyterlab-desktop is already installed"
         exit 0
     fi
-fi
-
-## build the app
-yarn
-yarn build
-
-## install the app
-if [[ "${os}" == Linux ]]; then
-    sudo snap install --dangerous build/JupyterLab_${VERSION}_amd64.snap
-else
-    open build/JupyterLab_${VERSION}.dmg
+    wget https://github.com/jupyterlab/jupyterlab-desktop/releases/latest/download/JupyterLab-Setup-macOS-x64.dmg
+    hdiutil attach JupyterLab-Setup-macOS-x64.dmg
+    JDIR=$(find /Volumes -d 1 -name "JupyterLab*")
+    sudo cp -R "${JDIR}/JupyterLab.app" /Applications
+    hdiutil unmount "${JDIR}"
+    rm -f JupyterLab-Setup-macOS-x64.dmg
 fi
