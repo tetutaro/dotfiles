bceb2f56c927f0d7940d14cfb76924e587e77351
Tetsutaro Maruyama <tetsutaro.maruyama@gmail.com>
2024-05-03 19:46:54 +0900

    install JupyterLab Desktop from flatpak (linux)

--- jupyterlab/install.sh
+++ jupyterlab/install.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 ## check dependencies
 os=$(uname -s)
-if [[ "${os}" == "Linux" ]]; then
-    PIPX_PATH="${HOME}/.local/bin/pipx"
-else
-    PIPX_PATH="/usr/local/bin/pipx"
-fi
-if [[ ! -f ${PIPX_PATH} ]]; then
+if [[ -z $(command -v pipx) ]]; then
     echo "install pipx first"
     exit 1
 else
@@ -14,7 +9,7 @@ else
 fi
 
 ## jupyterlab on pipx
-if [[ ! -d "${HOME}/.local/pipx/venvs/jupyterlab" ]]; then
+if [[ -z $(command -v jupyter-lab) ]]; then
     pipx install jupyterlab
     pipx inject jupyterlab pip
     pipx runpip jupyterlab install black ruff jupyterlab-lsp python-lsp-server python-lsp-ruff jupyterlab-code-formatter jupyterlab-templates jupyterlab-widgets ipywidgets ipympl
@@ -51,22 +46,21 @@ ln -sf ${PWD}/user-settings ${HOME}/.jupyter/lab/user-settings
 
 ## check the app is installed
 if [[ "${os}" == "Linux" ]]; then
-    if [[ -z $(command -v jlab) ]]; then
+    ret=$(/usr/bin/flatpak list | grep JupyterLab)
+    if [[ "x${ret}" == "x" ]]; then
+        /usr/bin/flatpak install flathub org.jupyter.JupyterLab
+    else
         echo "jupyterlab-desktop is already installed"
-        exit 0
     fi
-    wget https://github.com/jupyterlab/jupyterlab-desktop/releases/latest/download/JupyterLab-Setup-Debian.deb
-    sudo dpkg -i JupyterLab-Setup-Debian.deb
-    rm -f JupyterLab-Setup-Debian.deb
 else
     if [[ -d "/Applications/JupyterLab.app" ]]; then
         echo "jupyterlab-desktop is already installed"
-        exit 0
+    else
+        wget https://github.com/jupyterlab/jupyterlab-desktop/releases/latest/download/JupyterLab-Setup-macOS-x64.dmg
+        hdiutil attach JupyterLab-Setup-macOS-x64.dmg
+        JDIR=$(find /Volumes -d 1 -name "JupyterLab*")
+        sudo cp -R "${JDIR}/JupyterLab.app" /Applications
+        hdiutil unmount "${JDIR}"
+        rm -f JupyterLab-Setup-macOS-x64.dmg
     fi
-    wget https://github.com/jupyterlab/jupyterlab-desktop/releases/latest/download/JupyterLab-Setup-macOS-x64.dmg
-    hdiutil attach JupyterLab-Setup-macOS-x64.dmg
-    JDIR=$(find /Volumes -d 1 -name "JupyterLab*")
-    sudo cp -R "${JDIR}/JupyterLab.app" /Applications
-    hdiutil unmount "${JDIR}"
-    rm -f JupyterLab-Setup-macOS-x64.dmg
 fi
