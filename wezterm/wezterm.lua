8631ac81eccb9115b80b1fcc2269e45e31b24d35
Tetsutaro Maruyama <tetsutaro.maruyama@gmail.com>
2025-12-15 22:46:17 +0900

    disable ligature

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,9 +10,14 @@ config.font_size = 16
 config.font = wezterm.font('0xProGen Nerd Regular')
 config.line_height = 1.0
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
+config.harfbuzz_features = {
+    "calt=0",
+    "clig=0",
+    "liga=0",
 }
 return config
